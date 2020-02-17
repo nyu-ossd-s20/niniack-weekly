4575c694ff860c7bd042f95bcc194d5be5501fd2
niniack <nsa325@nyu.edu>
2020-02-17 15:58:51 -0500

    automated openstreet map contributions

---
+++ osmUpdate.py
@@ -0,0 +1,21 @@
+import osmapi
+api = osmapi.OsmApi()
+data = api.ChangesetsGet(username='niniack')
+changesetIDs = list(data)
+
+# print(changesetIDs)
+
+for i in range(len(changesetIDs)):
+    with open('contributions.md','a+') as f:
+        if (str(changesetIDs[i]) not in f.read()):
+
+            id = changesetIDs[i]
+
+            f.write('| {} |\
+            https://www.openstreetmap.org/changeset/{} | \
+                    OpenStreetMap | \
+                    {} |\
+                    '.format(data[id]['created_at'].strftime('%b %d'),
+                             data[id]['id'],
+                             data[id]['tag']['comment']
+                             ))
